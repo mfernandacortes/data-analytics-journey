c9b1f60b0270e0bfaa1ea1ba9d2a77f80df4f38f
María Fernanda Cortés <mfercortes@gmail.com>
2026-03-30 08:07:00 -0300

    Actualiza material SQL y ejercicios de análisis de datos

---
+++ Fundamentos/exists-not exists/in-not in.sql
@@ -0,0 +1,38 @@
+--lista de clientes que hicieron pedidos
+use Northwind;
+
+
+Select c.CustomerID, c.CompanyName from Customers c
+	where c.CustomerID in(select o.CustomerID from Orders o)
+
+select c.CompanyName from Customers c where c.CustomerID
+	not in(select o.CustomerID from Orders o)
+
+Select ProductName, AVG(UnitPrice) as prom
+from Products 
+where UnitPrice > AVG(UnitPrice)
+
+
+Select ProductID, ProductName, AVG(UnitPrice) as prom
+from Products  where (select UnitPrice > prom from Products)
+
+Select ProductID, ProductName, UnitPrice
+from Products
+where UnitPrice > (
+    select AVG(UnitPrice)
+    from Products
+)
+
+select o.OrderID from Orders o
+where o.OrderID > (select sum(od.quantity) as cantidades from [Order Details] od
+			group by o.OrderID
+			order by cantidades desc)
+			
+
+select o.OrderID
+from Orders o
+where (
+    select sum(od.Quantity)
+    from [Order Details] od
+    where od.OrderID = o.OrderID
+) > 100
