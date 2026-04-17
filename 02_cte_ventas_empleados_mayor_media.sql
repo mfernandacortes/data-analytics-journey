3f285509d96171cc6f83da7a9e271c7e38ae8cfe
mfernandacortes <mfercortes@gmail.com>
2026-04-17 07:51:32 -0300

    Add SQL script to find employees with high sales
    This SQL script retrieves employees with total sales above the overall average using CTEs.

---
+++ 02_cte_ventas_empleados_mayor_media.sql
@@ -0,0 +1,19 @@
+-- Empleados con ventas totales por encima del promedio general
+-- Dataset: Northwind | Tablas: Employees, Orders, Order Details
+-- Técnica: CTEs encadenadas + subquery en WHERE
+
+WITH  Empleados_mayores_ventas AS(
+	Select e.EmployeeID, e.LastName, sum(od.UnitPrice * od.Quantity * (1- 	od.discount)) as Monto
+	from Employees e
+	Join Orders o 
+	ON e.EmployeeID=o.EmployeeID
+	join [Order Details] od
+	ON o.OrderID=od.OrderID
+	group by e.EmployeeID, e.LastName
+),
+  promedio_gral AS(
+	Select AVG(Monto) as prom from Empleados_mayores_ventas
+
+)
+  select * from Empleados_mayores_ventas, promedio_gral where Monto > prom
+  order by Monto desc
