10f5126b225369fc03818416d735b5775c7706fb
María Fernanda Cortés <mfercortes@gmail.com>
2026-05-12 08:03:57 -0300

    Semana 4 - Pandas: groupby, size, merge con Orders

--- semana-04/03_pandas_sort_groupby.py
+++ semana-04/03_pandas_sort_groupby.py
@@ -23,3 +23,25 @@ clientes.to_csv('clientes_limpios.csv', index=False)
 
 # ordenar ahora por dos columnas
 print(clientes.sort_values(['Country', 'CompanyName'], ascending=True))
+
+"""Pasar de SQL a pandas: SELECT Country, COUNT(*) 
+FROM Customers 
+GROUP BY Country"""
+
+print(clientes.groupby('Country').size())
+
+#Ahora agregar el orden de mayor a menor:
+
+print(clientes.groupby('Country').size().sort_values(ascending=False))
+
+#SELECT Country, SUM(UnitPrice * Quantity) as total
+#FROM Orders
+#GROUP BY Country
+
+ordenes = pd.read_sql("SELECT * FROM Orders", conn)
+
+print(ordenes.head())
+
+# el JOIN de SQL con customers y orders:
+clientes_ordenes = pd.merge(clientes, ordenes, on='CustomerID')
+print(clientes_ordenes.head())
