f8b1ad4c6659de83aaf7caa2e6d0349ad7252131
María Fernanda Cortés <mfercortes@gmail.com>
2026-06-08 07:49:10 -0300

    Python basico - apply con axis=1 y función propia sobre Order Details

---
+++ python-basico/combinados3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sqlalchemy import create_engine 
+"""
+Sobre la tabla Order Details de Northwind — traé UnitPrice, Quantity y Discount, 
+y usá apply() con una función propia que calcule el monto real de cada fila (con 
+descuento incluido). Guardalo como columna nueva monto.
+
+"""
+# conexion
+engine = create_engine( 
+    "mssql+pyodbc://FERCHUSERVER/Northwind"    
+    "?driver=SQL+Server&trusted_connection=yes"
+
+)
+
+# traigo tablas que necesito:
+df = pd.read_sql("Select UnitPrice, Quantity, Discount from[Order Details]", engine)
+
+def calcular_monto(precio, cantidad, descuento):
+    monto = precio * cantidad * (1 - descuento)
+    return monto
+
+
+#lambda row: calcular_monto(row["UnitPrice"], row["Quantity"], row["Discount"])
+#df["Monto"] = df.apply(calcular_monto)
+
+# asi está bien:Cuando se necesita aplicar una función a cada fila usás axis=1. Sin él, 
+# apply() intenta recorrer columnas y no funciona para lo que se necesita. Línea correcta:
+
+df["Monto"] = df.apply(lambda row: calcular_monto(row["UnitPrice"], row["Quantity"], row["Discount"]), axis=1)
+print(df)
+# python combinados3.py
