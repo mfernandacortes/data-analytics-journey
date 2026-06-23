8cf001746aabb2476d9dc0d6ed157c6bb3a866a4
María Fernanda Cortés <mfercortes@gmail.com>
2026-06-23 07:42:13 -0300

    Python basico: concat con ignore_index, union de DataFrames Argentina y Brasil

---
+++ python-basico/concat_ej.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+clientes_arg = pd.DataFrame({
+    "CustomerID": ["ARG1", "ARG2", "ARG3"],
+    "Country": ["Argentina", "Argentina", "Argentina"],
+    "Revenue": [1500, 2300, 800]
+})
+
+clientes_bra = pd.DataFrame({
+    "CustomerID": ["BRA1", "BRA2"],
+    "Country": ["Brazil", "Brazil"],
+    "Revenue": [3200, 1100]
+})
+
+df_total = pd.concat([clientes_arg, clientes_bra], ignore_index=True)
+
+print(df_total)
+
+#  python concat_ej.py
