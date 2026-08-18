6df134d0d9019fc1b4ad449b94cc2a3c1fc8bcde
María Fernanda Cortés <mfercortes@gmail.com>
2026-08-18 07:56:34 -0300

    Merge Customers-Orders-OrderDetails + monto para pivot por cliente

---
+++ pandas-avanzado/pivot11.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sqlalchemy import create_engine 
+
+# conexión, descomentar según de donde trabaje, por defecto es la de escritorio
+engine = create_engine( 
+    # ESCRITORIO:
+     "mssql+pyodbc://FERCHUSERVER/Northwind?driver=SQL+Server&trusted_connection=yes"
+    # NOTEBOOK:
+    # "mssql+pyodbc://.\\SQLEXPRESS/Northwind?driver=SQL+Server&trusted_connection=yes"
+
+)
+
+"""
+CONSIGNA:
+Cliente: "Quiero ver, por cliente, cuántos pedidos hizo en total 
+y cuál fue su ticket promedio."
+"""
+
+# traer tablas:
+c=pd.read_sql("Select CustomerID, COmpanyName from Customers", engine)
+o=pd.read_sql("select OrderID, CustomerID from Orders", engine)
+od=pd.read_sql("select OrderID, Quantity,UnitPrice,Discount from [Order Details]", engine)
+
+
+# merge:
+co=pd.merge(c,o,on="CustomerID")
+co_od=pd.merge(co,od,on="OrderID")
+
+# creo una columna llamada monto:
+co_od["monto"]=co_od["Quantity"] * co_od["UnitPrice"] * (1 - co_od["Discount"])
+print(co_od)
+
+
+# pivot:
+
+# python pivot11.py
+
