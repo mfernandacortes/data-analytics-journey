7a7cc9f422f2fc715390536d4e8c2608d8bee783
María Fernanda Cortés <mfercortes@gmail.com>
2026-09-17 08:07:54 -0300

    Pandas: grafico matplotlib - ventas mensuales por mes/anio (Northwind)

--- pandas-avanzado/grafico1.py
+++ pandas-avanzado/grafico1.py
@@ -26,9 +26,21 @@ o_od=pd.merge(o,od,on="OrderID")
 
 # calcular monto:
 o_od["monto"]=o_od["Quantity"] * o_od["UnitPrice"] * (1 - o_od["Discount"])
+df=o_od.copy()
 
+# mes:
+df['anio'] = df['OrderDate'].dt.year
+df['mes'] = df['OrderDate'].dt.month
+ventas_mensuales = df.groupby(['anio', 'mes'])['monto'].sum()
+
+# gráfico
+import matplotlib.pyplot as plt
+
+plt.plot(ventas_mensuales.values)
+plt.title('Ventas mensuales')
+plt.show()
 # python grafico1.py
-print(o_od)
+
 
 """
 HALLAZGO:
