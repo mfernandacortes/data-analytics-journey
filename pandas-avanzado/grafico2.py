0b378db5a9e302c636e80fa0fd39450bf10fd04e
María Fernanda Cortés <mfercortes@gmail.com>
2026-09-19 09:42:31 -0300

    Pandas: grafico barras - merge base categorias (Northwind)

---
+++ pandas-avanzado/grafico2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine 
+
+# conexión, descomentar según de donde trabaje, por defecto es la de escritorio
+engine = create_engine( 
+    # ESCRITORIO:
+     "mssql+pyodbc://FERCHUSERVER/Northwind?driver=SQL+Server&trusted_connection=yes"
+    # NOTEBOOK:
+    # "mssql+pyodbc://.\\SQLEXPRESS/Northwind?driver=SQL+Server&trusted_connection=yes"
+
+)
+
+"""
+CONSIGNA:
+Graficar el total de ventas por categoría de producto en un gráfico de barras, 
+usando plt.bar().
+
+"""
+
+# traer tablas:
+ca=pd.read_sql("select CategoryID, CategoryName from Categories", engine)
+p=pd.read_sql("select ProductID, CategoryID, ProductName from Products", engine)
+od=pd.read_sql("select ProductID, Quantity, UnitPrice, Discount from [Order Details]", engine)
+
+
+# merge:
+ca_p=pd.merge(ca,p,on="CategoryID")
+df=pd.merge(ca_p, od, on="ProductID")
+
+# calcular monto:
+df["monto"]=df["Quantity"] * df["UnitPrice"] * (1 - df["Discount"])
+print(df)
+# python grafico2.py
+
+
+"""
+HALLAZGO:
+
+"""
